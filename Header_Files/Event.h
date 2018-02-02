04029cce552e56ebbe7464d2d8d7a3c15a5b624d
qk8333 <evan.smith.m@gmail.com>
2018-02-02 12:38:10 -0800

    Created Event.h
    The ostream friend operator works for now but will probably be overwritten.

---
+++ Header_Files/Event.h
@@ -0,0 +1,27 @@
+#ifndef EVENT_H
+#define EVENT_H 1
+
+#include<chrono>
+#include<ctime>
+#include<iostream>
+
+class Event
+{
+	public:
+		Event(std::chrono::seconds alrm, std::string lbl) : alarm(alrm), label(lbl) {}
+		std::chrono::seconds getAlarm() const {return alarm;}
+		std::string getLabel() const {return label;}
+		friend std::ostream &operator<<(std::ostream &os, Event const &e);
+		~Event() {}
+	private:
+		std::chrono::seconds alarm;
+		std::string label;
+};
+
+std::ostream &operator<<(std::ostream &os, Event const &e)
+{
+	std::time_t t = e.alarm.count();
+	return os << "Alert: " << std::ctime(&t) << "  " << e.getLabel() << std::endl;
+}
+
+#endif
