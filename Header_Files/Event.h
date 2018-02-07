8b52efe3fd2fd62da9d418fc702cf21352599dc6
qk8333 <evan.smith.m@gmail.com>
2018-02-07 12:21:12 -0800

    Update Event.h: Overloaded the '=' operator

--- Header_Files/Event.h
+++ Header_Files/Event.h
@@ -1,6 +1,6 @@
 #ifndef EVENT_H
 #define EVENT_H 1
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS 1
 
 #include<chrono>
 #include<ctime>
@@ -9,9 +9,11 @@
 class Event
 {
 	public:
+		Event();
 		Event(std::chrono::seconds alrm, std::string lbl) : alarm(alrm), label(lbl) {}
 		std::chrono::seconds getAlarm() const {return alarm;}
 		std::string getLabel() const {return label;}
+		Event operator=(const Event &e);
 		friend std::ostream &operator<<(std::ostream &os, Event const &e);
 		~Event() {}
 	private:
@@ -19,10 +21,23 @@ class Event
 		std::string label;
 };
 
+Event::Event() : label("")
+{
+	std::chrono::seconds a (8 * 60 * 60);
+	alarm = a;
+}
+
+Event operator=(const Event &lhs, const Event &rhs)
+{
+	lhs.alarm = rhs.alarm;
+	lhs.label = rhs.label;
+	return;
+}
+
 std::ostream &operator<<(std::ostream &os, Event const &e)
 {
 	std::time_t t = e.alarm.count();
-	return os << "Alert: " << std::ctime(&t) << "  " << e.getLabel() << std::endl;
+	return os << "Alert: " << std::ctime(&t) << "" << e.getLabel() << std::endl;
 }
 
 #endif
