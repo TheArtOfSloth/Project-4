4cd3dc83a1cca9f0009156518e68d277966177a5
Dathr <Dathr@users.noreply.github.com>
2018-02-08 12:00:48 -0800

    Made the loop work
    it works. now we only need to call the functions Evan and John wrote

--- yieldToMe.cpp
+++ yieldToMe.cpp
@@ -7,17 +7,75 @@
 #include<thread>
 using namespace std;
 
+bool alarmGoingOff = false;
+bool isRunning = true;
+bool placeholder;	//holds place of comparison statements
+
 void usering(int a)
 {
-	cout << endl << "1 = again, else quit: " << endl;
-	cin >> a;
-	switch (a)
+	while (isRunning)
 	{
-	case 1:
-		usering(a);
-		break;
-	default:
-		break;
+		if (alarmGoingOff)
+		{
+			cout << "Alarm going off! 1=snooze, 2=dismiss, other=exit" << endl;
+			cin >> a;
+			cout << endl;
+			if (a == 1)
+			{
+				alarmGoingOff = false;
+				//set alarm time for about 3 minutes in the future
+			}
+			else if (a == 2)
+			{
+				alarmGoingOff = false;
+				//delete alarm event
+			}
+			else
+			{
+				alarmGoingOff = false;	//to stop the other thread
+				isRunning = false;	//to exit both threads
+			}
+		}
+		else
+		{
+			cout << "1=view next alarm, 2=add new alarm," << endl;
+			cout << "3=delete next alarm, 4 = exit program." << endl;
+			cin >> a;
+			cout << endl;
+			switch (a)
+			{
+			case 1:
+			{
+				//view next alarm
+				break;
+			}
+			case 2:
+			{
+				//add new alarm
+				break;
+			}
+			case 3:
+			{
+				//delete next alarm
+				break;
+			}
+			case 4:
+			{
+				isRunning = false;
+				break;
+			}
+			case 5:
+			{
+				placeholder = true;
+				break;
+			}
+			default:
+			{
+				isRunning = false;
+				break;
+			}
+			}
+		}
 	}
 };
 
@@ -25,26 +83,30 @@ void alarming()
 {
 	using chrono::system_clock;
 	time_t tt = system_clock::to_time_t(system_clock::now());
-
 	struct tm * ptm = localtime(&tt);
-	cout << "Current time: " << put_time(ptm, "%X") << '\n';
-
-	cout << "Waiting for the next minute to begin...\n";
-	++ptm->tm_min; ptm->tm_sec = 0;
-	this_thread::sleep_until(system_clock::from_time_t(mktime(ptm)));
-
-	for (int i = 0; i < 5; i++)
+	while (isRunning)
 	{
-		cout << '\a';
+		tt = system_clock::to_time_t(system_clock::now());
+		ptm = localtime(&tt);
+		if (placeholder)	//if current time==alarm time
+		{
+			alarmGoingOff = true;
+			placeholder = false;
+		}
+		while (alarmGoingOff)
+		{
+			cout << '\a';
+			this_thread::sleep_for(chrono::seconds(1));
+		}
+		this_thread::sleep_for(chrono::seconds(1));
 	}
-	cout << "It's time!" << endl;
 };
 
 int main()
 {
 	int b;
-	thread first (alarming);
-	thread second (usering, b);
+	thread first(alarming);
+	thread second(usering, b);
 	cout << "ready as ever..." << endl;
 	first.join();
 	second.join();
