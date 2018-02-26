b6424f574540be0ccdc6a23bf0d9c73af2873626
Dathr <Dathr@users.noreply.github.com>
2018-02-25 20:12:08 -0800

    Update Poller.h

--- Header_Files/Poller.h
+++ Header_Files/Poller.h
@@ -155,15 +155,92 @@ void poller::viewNextAlarm()
 //runs user input
 void poller::userLoop()
 {
+	while (isRunning)
+	{
+		if (alarmGoingOff)
+		{
+			cout << "ALARM TIME!!! type any entry to exit" << endl;
+			cin >> command1;
+			alarmGoingOff = false;
+			deleteNextAlarm();
+		}
+		else
+		{
+			cout << "1 - view next alarm" << endl << "2 - add new alarm" << endl;
+			cout << "3 - delete next alarm" << endl << "4 - exit" << endl;
+			cout << "5 - test dummy alarm" << endl;
+			cin >> command1;
+			cout << endl;
+			switch (command1)
+			{
+			case 1:
+			{
+				viewNextAlarm();
+				break;
+			};
+			case 2:
+			{
+				addAlarm();
+				break;
+			};
+			case 3:
+			{
+				deleteNextAlarm();
+				break;
+			};
+			case 4:
+			{
+				isRunning = false;
+				break;
+			};
+			case 5:
+			{
+				placeholder = true;
+				break;
+			};
+			default:
+			{
+				cout << "Invalid option. Try again." << endl;
+				break;
+			};
+			};
+		}
+	};
 };
 
 //runs the alarm
 void poller::alarmLoop()
 {
+	using chrono::system_clock;
+	time_t tt = system_clock::to_time_t(system_clock::now());
+	struct tm * ptm = localtime(&tt);
+	while (isRunning)
+	{
+		tt = system_clock::to_time_t(system_clock::now());
+		ptm = localtime(&tt);    //Syntax error
+		if (placeholder/* || conditions are met*/)
+		{
+			placeholder = false;
+			alarmGoingOff = true;
+		};
+		if (alarmGoingOff)
+		{
+			while (alarmGoingOff)
+			{
+				cout << '\a';
+				this_thread::sleep_for(chrono::seconds(1));
+			};
+		}
+		this_thread::sleep_for(chrono::seconds(1));
+	}
 };
 
 //runs the threads
 void poller::pollingLoop()
 {
+	t1 = thread(&poller::userLoop, this);
+	t2 = thread(&poller::alarmLoop, this);
+	t1.join();
+	t2.join();
 };
 #endif
